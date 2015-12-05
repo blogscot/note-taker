616c71172a32cf27f8152a84586737608f9f6256
blogscot <iain.diamond@gmail.com>
2015-12-05 12:10:38 +0000

    Add basic drawing notes feature

--- app/src/main/java/iain/diamond/com/testapp/DrawActivity.java
+++ app/src/main/java/iain/diamond/com/testapp/DrawActivity.java
@@ -1,22 +1,45 @@
 package iain.diamond.com.testapp;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class DrawActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DrawActivity extends AppCompatActivity implements View.OnClickListener{
+
+  private final String TAG = this.getClass().getSimpleName();
+
+  private static Handler hander;
+  private DrawingView drawView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_draw);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
 
+    drawView = (DrawingView) findViewById(R.id.drawing);
+    Button clearButton = (Button) findViewById(R.id.clearButton);
+    Button saveButton = (Button) findViewById(R.id.saveButton);
+    clearButton.setOnClickListener(this);
+    saveButton.setOnClickListener(this);
+
+    hander = new Handler() {
+      @Override
+      public void handleMessage(Message msg) {
+        Toast.makeText(DrawActivity.this, "Drawing saved.", Toast.LENGTH_LONG).show();
+      }
+    };
   }
 
   @Override
@@ -26,4 +49,47 @@ public class DrawActivity extends AppCompatActivity {
     return true;
   }
 
+  @Override
+  public void onClick(View view) {
+
+    switch (view.getId()) {
+      case R.id.clearButton:
+        drawView.startNew();
+        break;
+      case R.id.saveButton:
+        drawView.setDrawingCacheEnabled(true);
+        String imageFilename = "IMAGE123.png";
+        storeImage(drawView.getDrawingCache(), getFilesDir() + "/" + imageFilename);
+        break;
+    }
+  }
+
+  // Stores a image bitmap to the full path specified by filename
+  private void storeImage(final Bitmap image, final String filename) {
+
+    Runnable storeInBackground = new Runnable() {
+      @Override
+      public void run() {
+
+        if (filename.equals("")) {
+          throw new IllegalArgumentException("storeImage Error: Invalid filename given");
+        }
+        Log.d(TAG, filename);
+        File pictureFile = new File(filename);
+        try {
+          FileOutputStream fos = new FileOutputStream(pictureFile);
+          image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+          fos.close();
+          hander.sendEmptyMessage(0);
+//      noteHandler.updateNotes();
+        } catch (FileNotFoundException e) {
+          Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+          Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+      }
+    };
+    Thread thread = new Thread(storeInBackground);
+    thread.start();
+  }
 }
---
+++ app/src/main/java/iain/diamond/com/testapp/DrawingView.java
@@ -0,0 +1,80 @@
+package iain.diamond.com.testapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View {
+
+  private Path drawPath;
+  private Paint drawPaint, canvasPaint;
+  private Canvas drawCanvas;
+  private Bitmap canvasBitmap;
+
+  public DrawingView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    setFocusable(true);
+    drawPath = new Path();
+    drawPaint = new Paint();
+
+    drawPaint.setColor(Color.BLACK);
+    drawPaint.setAntiAlias(true);
+    drawPaint.setStrokeWidth(10);
+    drawPaint.setStyle(Paint.Style.STROKE);
+    drawPaint.setStrokeJoin(Paint.Join.ROUND);
+    drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+    canvasPaint = new Paint(Paint.DITHER_FLAG);
+  }
+
+  public void startNew(){
+    drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+    invalidate();
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+
+    canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+    drawCanvas = new Canvas(canvasBitmap);
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+    canvas.drawPath(drawPath, drawPaint);
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+
+    float touchX = event.getX();
+    float touchY = event.getY();
+
+    switch (event.getAction()) {
+      case MotionEvent.ACTION_DOWN:
+        drawPath.moveTo(touchX, touchY);
+        break;
+      case MotionEvent.ACTION_MOVE:
+        drawPath.lineTo(touchX, touchY);
+        break;
+      case MotionEvent.ACTION_UP:
+        drawCanvas.drawPath(drawPath, drawPaint);
+        drawPath.reset();
+        break;
+      default:
+        return false;
+    }
+    invalidate();
+    return true;
+  }
+}
